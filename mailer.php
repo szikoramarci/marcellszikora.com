d478208ff851a661081fb5f1a35f37d222c2f67e
Szikora Marcell <szikora.marcell@webness.hu>
2024-12-11 17:32:23 +0100

    modified files

--- mailer.php
+++ mailer.php
@@ -1,20 +1,41 @@
 <?php
 
+require_once "Mail.php"; // Include the PEAR Mail package
+
+$headers = array(
+    'From' => 'test@ses.',
+    'To' => 'simabeats@gmail.com',
+    'Subject' => 'TTTT'
+);
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+$mail = Mail::factory("smtp", array(
+    'host' => 'marcell.solutions',
+    'port' => 465,
+    'auth' => true,
+    'username' => 'hello@marcell.solutions',
+    'password' => '6mH}YQL&5250'
+));
+
+// Send the email
+$result = $mail->send('simabeats@gmail.com', $headers, 'TESTT');
+var_dump($result);
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-        $mail_to = "povoyit226@anawalls.com";
+        $mail_to = "simabeats@gmail.com";
         
         # Sender Data
         $name    = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["full-name"])));
         $email   = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $phone   = $_POST["phone-number"];
-        $subject = $_POST["subject"];
-        $budget  = $_POST["budget"];
-        $file    = $_POST["file"];
-        $message = trim($_POST["message"]);
+        $message = $_POST["message"] ? trim($_POST["message"]) : '';
         
-        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($message)) {
+        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             # Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo "Please complete the form and try again.";
@@ -23,18 +44,14 @@
         
         # Mail Content
         $content = "Name: $name\n";
-        $content .= "Subject: $subject\n\n";
-        $content .= "Email: $email\n\n";
-        $content .= "Phone: $phone\n\n";
-        $content .= "Budget: $budget\n\n";
         $content .= "Message:\n$message\n";
-        $content .= "File: <a href='$file' target='_blank'>$file</a>";
 
         # email headers.
         $headers = "From: $name <$email>";
 
         # Send the email.
-        $success = mail($mail_to, $name, $content, $headers);
+        $success = mail($mail_to, 'New message from marcell.solutions', $content, $headers);
+        var_dump($success);
         if ($success) {
             # Set a 200 (okay) response code.
             http_response_code(200);
